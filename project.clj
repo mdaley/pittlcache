da853d7b92ea0b5e9065cef6eeeda998c4e860aa
Matthew Daley <matt.daley@blueyonder.co.uk>
2015-07-05 16:38:21 +0100

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject pittlcache "0.1.0-SNAPSHOT"
+  :description "Simple per item TTL cache based on clojure/core.cache"
+  :url "http://github.com/mdaley/pittlcache"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
